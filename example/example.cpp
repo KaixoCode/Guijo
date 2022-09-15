7b374643d36d0781867e88949b27c47556ac0104
KaixoCode <jeroenzwiers2@gmail.com>
2022-09-15 19:18:50 +0200

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -1,186 +1,183 @@
 #include "Guijo/Guijo.hpp"
+#include <ranges>
 
 using namespace Guijo;
 
-/*
-TODO:
- - custom logger
-*/
+struct Beacon : Object {
+    Point<double> location{ 0, 0 };
+    double distance = 100;
+    bool enabled = true;
 
-class MyObject : public Object {
-public:
+    bool dragging = false;
+    bool adjusting = false;
+
     StateLinked<Animated<Color>> color;
 
-    MyObject(Color c) : color{ { c, 0., Curves::easeOut<2> } } {
+    Beacon() {
+        box.use = false;
         link(color);
 
-        color[Focused] = c.brighter(1.6);
-        color[Hovering] = c.brighter(1.3);
+        location.x(rand() % 500);
+        location.y(rand() % 500);
 
-        box.size.height.transition(0.f);
-        box.size.height[Hovering] = 300;
+        Color _c = HSV<float>{ static_cast<float>(rand() % 50) * 5, 128.f, 255.f, 100.f };
+        color = _c;
+        color[Hovering] = _c.brighter(0.8);
+
+        event<Beacon>();
     }
 
-    void draw(DrawContext& context) const override {
-        context.fill(color);
-        context.rect(dimensions());
-        Object::draw(context);
-    }
-};
-
-struct WindowStyle : Flex::Class {
-    WindowStyle() {
-        flex.wrap = Flex::Wrap::NoWrap;
-        flex.direction = Flex::Direction::Column;
-        justify = Flex::Justify::Start;
-        align.content = Flex::Align::Stretch;
-        align.items = Flex::Align::Stretch;
-        padding = 15.f;
-    }
-};
-
-struct Class1 : Flex::Class {
-    Class1() {
-        size.width = Flex::Value::Auto;
-        size.height = 200;
-        margin = 10.f;
-        flex.wrap = Flex::Wrap::NoWrap;
-        flex.direction = Flex::Direction::Row;
-        flex.grow = 0;
-        flex.shrink = 0;
-        justify = Flex::Justify::Start;
-        align.content = Flex::Align::Stretch;
-        align.items = Flex::Align::Stretch;
-    }
-};
-
-struct Class11 : Flex::Class {
-    Class11() {
-        size.height = Flex::Value::Auto;
-        size.width = 30.f;
-        margin = 5.f;
-        flex.grow = 0;
-        flex.shrink = 0;
-        align.self = Flex::Value::Auto;
-    }
-};
-
-
-
-
-class Button : public Object, public StateListener {
-public:
-    struct Behaviour : public Refcounted {
-        virtual void trigger() = 0;
-    };
-
-    struct Click : public Behaviour {
-        const Click(std::invocable auto const& fun) : fun(fun) {}
-        std::function<void()> fun;
-        void trigger() override { fun(); }
-    };
-
-    struct Toggle : public Behaviour {
-        const Toggle(std::invocable<bool> auto const& fun) : fun(fun) {}
-        std::function<void(bool)> fun;
-        bool state = false;
-        void trigger() override { fun(state ^= true); }
-    };
-
-    struct Graphics : public Refcounted, public StateListener {
-        void update(StateId id, State value) override { for (auto& i : listeners) i->update(id, value); }
-        virtual void draw(const Dimensions<float>& dims, DrawContext& context) const = 0;
-        template<std::derived_from<StateListener> Ty>
-        void link(Ty& val) { listeners.push_back(&val); }
-    private:
-        std::vector<StateListener*> listeners{};
-    };
-
-    struct Default : public Graphics {
-        StateLinked<Animated<Color>> fill{};
-        StateLinked<Animated<Color>> border{};
-        StateLinked<Animated<Color>> color{};
-        StateLinked<Animated<float>> borderWidth{};
-        float fontSize = 20.f;
-        std::string text = "Button";
-        std::string font{ Font::Default };
-
-        Default() {
-            border = Color{ 95.f };
-            borderWidth = 2.f;
-            fill.curve(Curves::easeOut<5>);
-            fill.transition(200.f);
-            fill = Color{ 45.f };
-            fill[Pressed] = Color{ 75.f };
-            fill[Hovering] = Color{ 55.f };
-            color = Color{ 255.f };
-            link(fill);
-            link(border);
-            link(color);
-            link(borderWidth);
+    void mousePress(const MousePress& event) {
+        if (event.button & MouseButton::Left) {
+            dragging = true;
+        } else if (event.button & MouseButton::Right) {
+            adjusting = true;
+        } else if (event.button & MouseButton::Middle) {
+            enabled ^= true;
         }
-
-        void draw(const Dimensions<float>& dims, DrawContext& context) const override {
-            context.fill(fill);
-            context.stroke(border);
-            context.strokeWeight(borderWidth);
-            context.rect(dims);
-            context.textAlign(Align::Center);
-            context.fill(color);
-            context.font(font);
-            context.fontSize(fontSize);
-            context.text(text, dims.center());
-        }
-    };
-
-    Button() { 
-        event<Button>();
-        link(*this);
     }
 
-    Button(std::derived_from<Behaviour> auto const& behaviour)
-        : behaviour(new std::decay_t<decltype(behaviour)>{ behaviour }) {
-        event<Button>();
-        link(*this);
+    void mouseRelease(const MouseRelease& event) {
+        dragging = false;
+        adjusting = false;
     }
 
-    void mouseRelease(const MouseRelease& e) {
-        if (hitbox(e.pos)) behaviour->trigger();
+    void mouseDrag(const MouseDrag& event) {
+        if (dragging) 
+            location = event.pos;
+        if (adjusting)
+            distance = event.pos.distance(location);
     }
 
-    void draw(DrawContext& context) const override {
-        graphics->draw(dimensions(), context);
+    bool hitbox(Point<float> pos) const override {
+        return pos.distance(location) < distance;
     }
 
-    Pointer<Behaviour> behaviour = new Click{ []() {} };
-    Pointer<Graphics> graphics = new Default{};
-private:
+    void pre(DrawContext& p) const override {}
+    void post(DrawContext& p) const override {}
 
-    void update(StateId id, State value) override {
-        if (graphics) graphics->update(id, value);
+    void draw(DrawContext& p) const override {
+        Object::draw(p);
+        if (enabled) {
+            p.fill(color);
+            p.circle(location, distance);
+        } 
+        p.fill({ 255, 255, 255, 255 });
+        p.circle(location, 3);
     }
+
+    void update() override {
+        pos(location);
+    }
+};
+
+struct Guess : Object {
+
+    Point<double> guess{ 300, 300 };
+
+    void pre(DrawContext& p) const override {}
+    void post(DrawContext& p) const override {}
+    void draw(DrawContext& p) const override {
+        p.fill({ 255, 0, 0, 255 });
+        p.circle(guess, 10);
+    }
+};
+
+struct PointWDist : Point<double> {
+    double distance;
 };
 
 int main() {
-    
-    Gui gui;
+    Gui _gui;
 
-    auto window = gui.emplace<Window>({
-        .name = "HelloWorld1",
-        .dimensions{ -1, -1, 800, 800 },
+    auto _window = _gui.emplace<Window>({
+        .name = "Test",
+        .dimensions{ -1, -1, 800, 500 },
     });
 
-    window->box = WindowStyle{};
-    window->box.use = true;
-    window->box.align.content = Flex::Align::Center;
-    window->box.align.items = Flex::Align::Center;
-    window->box.justify = Flex::Align::Center;
+    _window->box.use = false;
+    _window->box.size.width = Flex::pc{ 100 };
+    _window->box.size.height = Flex::pc{ 100 };
 
-    auto obj = window->emplace<Button>(Button::Toggle{ [](bool v) { std::cout << "click: " << v << "\n"; } });
-    obj->box.use = true;
-    obj->box.size = { 400, 50 };
+    std::vector<Pointer<Beacon>> beacons;
 
-    while (gui.loop());
+    beacons.push_back(_window->emplace<Beacon>());
+    beacons.push_back(_window->emplace<Beacon>());
+    beacons.push_back(_window->emplace<Beacon>());
+    beacons.push_back(_window->emplace<Beacon>());
+    beacons.push_back(_window->emplace<Beacon>());
 
-    return 0;
+    auto _guess = _window->emplace<Guess>();
+
+    while (_gui.loop()) {
+        using namespace std::views;
+
+        constexpr auto distancePointCircle = [](Point<double> point, Point<double> circle, double radius) {
+            return std::abs(std::sqrt(std::pow(point.x() - circle.x(), 2) + std::pow(point.y() - circle.y(), 2)) - radius);
+        };
+
+        double sumDistancesAtMouse = 0.0;
+        for (auto& beacon : beacons) {
+            if (!beacon->enabled) continue;
+            sumDistancesAtMouse += distancePointCircle(_window->cursor.position, beacon->location, beacon->distance);
+        }
+
+        std::cout << sumDistancesAtMouse << '\n';
+
+        auto filtered = filter(beacons, [](Pointer<Beacon>& beacon) { return beacon->enabled; });
+        auto minX = std::ranges::min(transform(filtered, [](Pointer<Beacon>& beacon) { return beacon->location.x(); }));
+        auto maxX = std::ranges::max(transform(filtered, [](Pointer<Beacon>& beacon) { return beacon->location.x(); }));
+        auto minY = std::ranges::min(transform(filtered, [](Pointer<Beacon>& beacon) { return beacon->location.y(); }));
+        auto maxY = std::ranges::max(transform(filtered, [](Pointer<Beacon>& beacon) { return beacon->location.y(); }));
+        auto h = std::max(maxX - minX, maxY - minY) * 3.;
+        auto center = Point<double>{ minX + (maxX - minX) / 2., minY + (maxY - minY) / 2. };
+
+        std::vector<PointWDist> points{};
+
+        auto addAround = [&](Point<double>& p) {
+            double x = p.x();
+            double y = p.y();
+            double offsets[]{ 0.0, h / 3.0, -h / 3.0 };
+            for (auto xOffset : offsets) for (auto yOffset : offsets)
+                if (!(xOffset == 0.0 && yOffset == 0.0))
+                    points.push_back({ { x + xOffset, y + yOffset }, 0.0 });
+        };
+
+        points.push_back({ center, 0.0 });
+
+        constexpr std::size_t iterations = 8;
+
+        double smallestFoundDistance = 1e10;
+        for (std::size_t i = 0; i < iterations; ++i) {
+            std::size_t size = points.size();
+            for (std::size_t j = 0; j < size; ++j) {
+                addAround(points[j]);
+            }
+
+            h /= 3.0;
+
+            for (auto& point : points) {
+                double sumOfDistances = 0.0;
+                for (auto& beacon : beacons) {
+                    if (!beacon->enabled) continue;
+                    sumOfDistances += distancePointCircle(point, beacon->location, beacon->distance);
+                }
+
+                smallestFoundDistance = std::min(smallestFoundDistance, sumOfDistances);
+                point.distance = sumOfDistances;
+            }
+
+            for (auto _i = points.begin(); _i != points.end();) {
+                auto& point = *_i;
+                if (point.distance > smallestFoundDistance + (3 * h) / std::numbers::sqrt2) {
+                    _i = points.erase(_i);
+                } else ++_i;
+            }
+
+            smallestFoundDistance = 1e10;
+        }
+
+        _guess->guess = points[0];
+    }
 }
+
